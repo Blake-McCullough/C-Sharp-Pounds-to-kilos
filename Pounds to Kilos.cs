97bf1e6b4e9124cd103d4c82788d49d85a25e888
Blake-McCullough <66660752+Blake-McCullough@users.noreply.github.com>
2021-07-19 10:23:27 +0800

    Add files via upload

---
+++ Pounds to Kilos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* Program to covert pounds to kilos
+ * written by Blake McCullough
+ * Created 19 July 2019
+ * */
+
+namespace Pounds_to_Kilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Declaring Variables */
+
+            double pounds = 0;
+            double kilos = 0;
+            double convAmount = 2.2;
+            string input = "";
+
+
+            /* Gather pounds from user*/
+
+            Console.Write("Please Enter an amount in pounds:  ");
+            input = Console.ReadLine();
+
+            /*Converts string input into numerical variable*/
+
+            pounds = double.Parse(input);
+
+            /*Converted to kilos*/
+
+            kilos = pounds / convAmount;
+
+            /* Rounds to 2 decimal places */
+
+            kilos = Math.Round(kilos, 2);
+
+            /* Displaying output to user*/
+
+            Console.Write(pounds +" pounds is equal to " + kilos+ " kilos.");
+
+            /* Prevents from closing immediately*/
+            Console.ReadLine();
+        }
+    }
+}
